12285e07fa288292afb042f471896b132479f37a
Saravana <67310667+SaravanaNani@users.noreply.github.com>
2024-07-10 15:01:22 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -8,15 +8,15 @@ sudo apt update && sudo apt install openjdk-11-jdk unzip -y
 sudo mkdir -p /home/sonar
 
 cd /opt/
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.6.0.59041.zip
-unzip sonarqube-9.6.0.59041.zip
+wget https://binaries.sonarsource.com/Distribution/sonarqube/ sonarqube-9.9.6.92038.zip
+unzip sonarqube-9.9.6.92038.zip
 sudo useradd --shell /bin/bash sonar
-chown sonar:sonar /opt/sonarqube-9.6.0.59041 -R
-chmod 777 /opt/sonarqube-9.6.0.59041 -R
+chown sonar:sonar /opt/sonarqube-9.9.6.92038 -R
+chmod 777 /opt/ sonarqube-9.9.6.92038 -R
 su - sonar
 
 #run this on server manually
-#cd /opt/sonarqube-9.6.0.59041/bin/linux-x86-64/
+#cd /opt/ sonarqube-9.9.6.92038/bin/linux-x86-64/
 #sh sonar.sh start
 #sh sonar.sh status
 
