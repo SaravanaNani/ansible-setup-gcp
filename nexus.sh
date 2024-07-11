87a6ed0d67f2efe8554f680c0f19caa1fb5629e8
Saravana <67310667+SaravanaNani@users.noreply.github.com>
2024-07-11 15:46:22 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -9,3 +9,7 @@ sudo passwd nexus
 sudo chown -R nexus:nexus /app/nexus
 sudo chown -R nexus:nexus /app/sonatype-work
 echo 'run_as_user="nexus"' | sudo tee /app/nexus/bin/nexus.rc
+
+#Step2 – Start the nexus using the below command in nexus Instance SSH terminal
+# /app/nexus/bin/nexus start
+# /app/nexus/bin/nexus status
